49cd84fa19ac257f9b2c343280e92fc75c979cc1
Eric Shakya <erikkshakya@gmail.com>
2024-03-03 17:11:49 +0545

    minor fixes

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -23,12 +23,14 @@ export async function createUser(user: CreateUserParams) {
 export async function getUserById(userId: string) {
   try {
     await connectToDatabase();
-
+    console.log(userId, 'aaaa')
     const user = await User.findOne({ clerkId: userId });
+    console.log(user, 'aaaa1')
 
-    if (!user) throw new Error("User not found");
+    // if (!user) throw new Error("User not found");
+    // console.log(user, 'user')
 
-    return JSON.parse(JSON.stringify(user));
+    // return JSON.parse(JSON.stringify(user));
   } catch (error) {
     handleError(error);
   }
@@ -44,7 +46,7 @@ export async function updateUser(clerkId: string, user: UpdateUserParams) {
     });
 
     if (!updatedUser) throw new Error("User update failed");
-    
+
     return JSON.parse(JSON.stringify(updatedUser));
   } catch (error) {
     handleError(error);
@@ -80,11 +82,11 @@ export async function updateCredits(userId: string, creditFee: number) {
 
     const updatedUserCredits = await User.findOneAndUpdate(
       { _id: userId },
-      { $inc: { creditBalance: creditFee }},
+      { $inc: { creditBalance: creditFee } },
       { new: true }
     )
 
-    if(!updatedUserCredits) throw new Error("User credits update failed");
+    if (!updatedUserCredits) throw new Error("User credits update failed");
 
     return JSON.parse(JSON.stringify(updatedUserCredits));
   } catch (error) {
